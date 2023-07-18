0acf5dad4c7127cc7f3124085f6fb6b3ce04ba89
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-07-18 16:18:08 -0300

    update ex015

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -7,22 +7,35 @@ function verificar() {
         window.alert('Verifique se o ano foi digitado corretamente e tente novamente.');
     } else {
         var idade = ano_atual - Number(ano_user.value);
-        var img = window.document.getElementById('img');
         var gender = window.document.getElementsByName('radgender');
         var res = window.document.querySelector('div#result');
 
+        var sex = '';
+
+        if (gender[0].checked) {
+            sex = 'Masculino';
+        } else if (gender[1].checked) {
+            sex = 'Feminino';
+        }
+
+        res.innerHTML = `Gênero: ${sex}.<br>Idade em ${ano_atual}: ${idade}`;
+
+        var img = document.createElement('img');
+        img.setAttribute('id', 'img');
+        
         if (idade >= 0 && idade < 13) {
-            res.innerHTML = `Você faz ${idade} em ${ano_atual}.`;
-            gender[0].checked ? img.src = 'criança-menino.png' : img.src = 'criança-menina.png';
+            gender[0].checked ? img.setAttribute('src', 'imagens/criança-menino.png') : img.setAttribute('src', 'imagens/criança-menina.png');
         }
-        else if (idade >= 14 && idade < 18) {
-            res.innerHTML = `Você faz ${idade} em ${ano_atual}`;
+        else if (idade >= 14 && idade <= 21) {
+            gender[0].checked ? img.setAttribute('src', 'imagens/jovem-homem.png') : img.setAttribute('src', 'imagens/jovem-mulher.png');
         }
-        else if (idade >= 18 && idade < 60) {
-            res.innerHTML = `Você faz ${idade} em ${ano_atual}.<br>Você é um adulto!`;
+        else if (idade >= 22 && idade < 60) {
+            gender[0].checked ? img.setAttribute('src', 'imagens/adulto-homem.png') : img.setAttribute('src', 'imagens/adulta-mulher.png');
         }
         else {
-            res.innerHTML = `Você faz ${idade} em ${ano_atual}`;
+            gender[0].checked ? img.setAttribute('src', 'imagens/idoso-homem.png') : img.setAttribute('src', 'imagens/idosa-mulher.png');
         }
+
+        res.appendChild(img);
     }
 }
