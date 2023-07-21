deb63b2a7217ddc3049c3934a11d56e8e0820560
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-07-21 16:35:49 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,49 +1,66 @@
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    }
+    return false;
+}
+
+
+function inArray(n, v) {
+   // Se o índice do número n for identificável 
+   if (v.indexOf(Number(n)) != -1) return true;
+   return false;
+}
+
+
 function adicionarNumero() {
     let number = document.querySelector('input#txtnumber');
     let table = document.querySelector('select#tab');
 
-    if  (number.value.length == 0) {
-        window.alert('Insira um número para que ele seja adicionado à tabela.');
-        return;
+    if (isNumber(number.value) && !inArray(number.value, v)) {
+        let n = Number(number.value);
+
+        let opt = document.createElement('option');
+        v.push(n);
+        opt.text = `Número ${n} adicionado com sucesso!`;
+        table.appendChild(opt);
+
+        let res = document.querySelector('div#result');
+        res.innerHTML = `<p>Este é o vetor agora: [${v}]</p>`;
+    } else {
+        window.alert('Por favor insira um número que já não tenha sido adicionado previamente.')
     }
 
-    let n = Number(number.value);
-
-    if (n > 200 || n < 1) {
-        window.alert('Por favor, insira um número presente no intervalo entre 1 e 100.');
-        return;
-    }
-
-    let opt = document.createElement('option');
-    opt.text = '';
-    v.push(n);
-    opt.text = `Número ${n} adicionado com sucesso!`;
-    table.appendChild(opt);
-
-    let res = document.querySelector('div#result');
-    res.innerHTML = `<p>Este é o vetor agora: ${v}</p>`;
+    number.value = '';
+    number.focus();
 }
 
-function analise() {
-    let len = v.length;
 
-    if (len == 0) {
+function analise() {
+    if (v.length == 0) {
         window.alert('Não é possível analisar um vetor vazio.');
         return;
     }
 
-    let soma = v.sum;
+    let soma = 0;
+    let maior = v[0];
+    let menor = v[0];
+    
+    for (i in v) {
+        if (v[i] > maior) maior = v[i];
+        else if (v[i] < menor) menor = v[i];
+        soma += v[i];
+    }
 
     let result = document.querySelector('div#result');
 
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let p4 = document.createElement('p');
-
-    p1.innerHTML = `<p>O vetor possui tamanho ${len}</p>`;
-
-    result.appendChild(p1);
+    result.innerHTML = '';
+    result.innerHTML += `<p>Este é o vetor agora: [${v}]</p>`;
+    result.innerHTML += `<p>Quantidade de elementos no vetor: ${v.length}</p>`;
+    result.innerHTML += `<p>Maior elemento: ${maior}</p>`;
+    result.innerHTML += `<p>Menor elemento: ${menor}</p>`;
+    result.innerHTML += `<p>Soma dos elementos do vetor: ${soma}</p>`;
+    result.innerHTML += `<p>Média dos elementos: ${(soma/v.length).toFixed(2)}</p>`;
 }
 
 let envioNumero = window.document.querySelector("input#button");
