fcf78cd534be67f1e51e290b569271d2510a592d
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-07-21 17:21:10 -0300

    Create objects.js

---
+++ aula17/objects.js
@@ -0,0 +1,13 @@
+let amigo = {
+    nome: 'José', 
+    idade: 20, 
+    sexo: 'Masculino', 
+    peso: 80,
+    engordar(p = 0) {
+        console.log('Engordou!');
+        this.peso += p;
+    }
+};
+
+amigo.engordar(2);
+console.log(`Esse cara, o ${amigo.nome}, pesa ${amigo.peso}Kg`);
