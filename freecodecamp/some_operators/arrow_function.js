c1ba9dcd563deeabd8f9d531ac97a823c3bf7741
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-07-27 17:09:55 -0300

    arrow function lessons

--- freecodecamp/some_operators/arrow_function.js
+++ freecodecamp/some_operators/arrow_function.js
@@ -1,4 +1,17 @@
+/*
 let magic = function() {
     return new Date();
 }
+*/
 
+const magic = () => new Date();
+
+/*
+var myConcat = function(v1, v2) {
+    return v1.concat(v2);
+};
+*/
+
+const myConcat = (v1, v2) => v1.concat(v2);
+
+console.log(myConcat([1, 2, 3], [4, 5, 6]));
