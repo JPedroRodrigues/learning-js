a104976f11924463f9bc00a4a241d0e46265e54b
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-07-28 17:39:46 -0300

    create lesson files

---
+++ freecodecamp/some_operators/destructuring_assignment.js
@@ -0,0 +1,9 @@
+const voxel = {x:1, y:2, z:3};
+
+/*
+const a = voxel.x;
+const b = voxel.y;
+const c = voxel.z;
+*/
+
+const {x:a, y:b, z:c} = voxel;
---
+++ freecodecamp/some_operators/spread_operator.js
@@ -0,0 +1,6 @@
+let v1 = ['Jan', 'Fev', 'Mar'];
+let v2 = [...v1];
+v1[0] = 2023;
+
+console.log(v1);
+console.log(v2);
